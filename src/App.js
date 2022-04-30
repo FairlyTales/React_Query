905fb4e299aef5c052574397c8fa5917189ab014
khnychkin_kirill <khnychkin_kirill@tutanota.com>
2022-04-30 23:19:22 +0300

    prettified the project;

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
-import "./App.css";
-import {QueryClientProvider, QueryClient} from "react-query";
-import {Characters} from "./Characters";
+import './App.css';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Characters } from './Characters';
 
 const queryClient = new QueryClient();
 
 const App = () => {
-	return (
-		<div className="App">
-			<QueryClientProvider client={queryClient}>
-				<div className="container">
-					<h1>Rick and Morty characters</h1>
-					<Characters/>
-				</div>
-			</QueryClientProvider>
-		</div>
-	);
-}
+  return (
+    <div className='App'>
+      <QueryClientProvider client={queryClient}>
+        <div className='container'>
+          <h1>Rick and Morty characters</h1>
+          <Characters />
+        </div>
+      </QueryClientProvider>
+    </div>
+  );
+};
 
 export default App;
--- src/Characters.jsx
+++ src/Characters.jsx
@@ -1,41 +1,41 @@
-import React, {useState} from 'react';
-import {useQuery} from "react-query";
-import {Character} from "./Character";
+import React, { useState } from 'react';
+import { useQuery } from 'react-query';
+import { Character } from './Character';
 
 export const Characters = () => {
-	const [page, setPage] = useState(1);
+  const [page, setPage] = useState(1);
 
-	const fetchCharacters = async ({queryKey}) => {
-		const response = await fetch(`https://rickandmortyapi.com/api/character?page=${queryKey[1]}`)
-		return response.json();
-	}
+  const fetchCharacters = async ({ queryKey }) => {
+    const response = await fetch(`https://rickandmortyapi.com/api/character?page=${queryKey[1]}`);
+    return response.json();
+  };
 
-	const {data, status, isPreviousData} = useQuery(['characters', page], fetchCharacters, {
-		keepPreviousData: true,
-	})
+  const { data, status, isPreviousData } = useQuery(['characters', page], fetchCharacters, {
+    keepPreviousData: true,
+  });
 
-	if (status === 'loading') return <p>Loading...</p>
-	if (status === 'error') return <p>Error</p>
+  if (status === 'loading') return <p>Loading...</p>;
+  if (status === 'error') return <p>Error</p>;
 
-	return (
-		<div className='characters'>
-			{data.results.map(character => (
-				<Character key={character.id} character={character}/>
-			))}
-			<div>
-				<button
-					disabled={!data.info.prev}
-					onClick={() => setPage((prevPage) => prevPage - 1)}
-				>
-					Prev
-				</button>
-				<button
-					disabled={isPreviousData && !data.info.next}
-					onClick={() => setPage((prevPage) => prevPage + 1)}
-				>
-					Next
-				</button>
-			</div>
-		</div>
-	);
+  return (
+    <div className='characters'>
+      {data.results.map(character => (
+        <Character key={character.id} character={character} />
+      ))}
+      <div>
+        <button
+          disabled={!data.info.prev}
+          onClick={() => setPage((prevPage) => prevPage - 1)}
+        >
+          Prev
+        </button>
+        <button
+          disabled={isPreviousData && !data.info.next}
+          onClick={() => setPage((prevPage) => prevPage + 1)}
+        >
+          Next
+        </button>
+      </div>
+    </div>
+  );
 };
